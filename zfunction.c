f0551ff19a331cf5699082ca1e285910d7deabc5
whydoineedthisforclass <vbortolotto02@gmail.com>
2024-12-05 10:47:33 -0500

    Update zfunction.c

--- zfunction.c
+++ zfunction.c
@@ -12,30 +12,30 @@ float sub(float a, float b){
 float ad(float a, float b){
     return a+b;
 }
-//Square and Maximum for giggles
-float maximum(float a, float b){
-    if(a>b){
-        return a;
-    }else{
-        return b;
-    }
-}
+//Square and percent for giggles
 float squ(float a,float b){
-    int e,f;
+    float e;
     printf("What type of exponential function would you like? Ex: Square is 2 while cube is 3\n");
-    scanf("%d", &e);
+    scanf("%f", &e);
     for(int c=1; c<e; c++){
         b=b*a;
     }
     return b;
 }
+float per(float a){
+    float e,b;
+    printf("What percent off are you looking for ex: 80%% is 80 not 0.8\n");
+    scanf("%f", &e);
+    e=e/100;
+    b=a*e;
+    return b;
+}
 int main(){
     float num1, num2,z;
     char type;
-    printf("Enter what you want to do...*, /, +, or -\nexponential is e.\n");
-    printf(" Whatever you do...do not press x\n");
+    printf("Enter what you want to do...,%%,*, /, +, or -\n Exponential is e.\n");
     scanf("%c",&type);
-    printf("Enter number(s) you want to work with\n");
+    printf("Enter numbers you want to work with\n");
     switch(type){
         case '*':
             scanf("%f%f",&num1, &num2);
@@ -57,14 +57,14 @@ int main(){
             z= ad(num1,num2);
             printf("Answer is %f\n", z);
             break;
+        case '%':
+            scanf("%f",&num1);
+            z= per(num1);
+            printf("Answer is %f\n", z);
+            break;
         case 'e':
             scanf("%f",&num1);
             z= squ(num1,num1);
-            printf("Answer is %f\n", z);
-            break;
-        case 'm':
-            scanf("%f%f",&num1, &num2);
-            z= maximum(num1,num2);
             printf("Answer is %f\n", z);
             break;
     }
